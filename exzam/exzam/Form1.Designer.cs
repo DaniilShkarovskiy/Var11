e68e2a75aa7f9a69dd9fedb03a01d651f0e03c87
DaniilShkarovskiy <72796379+DaniilShkarovskiy@users.noreply.github.com>
2022-04-16 10:57:55 +0700

    Add files via upload

---
+++ exzam/exzam/Form1.Designer.cs
@@ -0,0 +1,133 @@
+﻿
+namespace exzam
+{
+	partial class Form1
+	{
+		/// <summary>
+		/// Обязательная переменная конструктора.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Освободить все используемые ресурсы.
+		/// </summary>
+		/// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Код, автоматически созданный конструктором форм Windows
+
+		/// <summary>
+		/// Требуемый метод для поддержки конструктора — не изменяйте 
+		/// содержимое этого метода с помощью редактора кода.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.components = new System.ComponentModel.Container();
+			this.label1 = new System.Windows.Forms.Label();
+			this.label2 = new System.Windows.Forms.Label();
+			this.label3 = new System.Windows.Forms.Label();
+			this.textBox1 = new System.Windows.Forms.TextBox();
+			this.textBox2 = new System.Windows.Forms.TextBox();
+			this.errorProvider1 = new System.Windows.Forms.ErrorProvider(this.components);
+			this.maskedTextBox1 = new System.Windows.Forms.MaskedTextBox();
+			((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).BeginInit();
+			this.SuspendLayout();
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+			this.label1.Location = new System.Drawing.Point(98, 129);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(171, 20);
+			this.label1.TabIndex = 0;
+			this.label1.Text = "Название рецепта:";
+			this.label1.Click += new System.EventHandler(this.label1_Click);
+			// 
+			// label2
+			// 
+			this.label2.AutoSize = true;
+			this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+			this.label2.Location = new System.Drawing.Point(148, 161);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(116, 20);
+			this.label2.TabIndex = 1;
+			this.label2.Text = "Ингредиент:";
+			// 
+			// label3
+			// 
+			this.label3.AutoSize = true;
+			this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+			this.label3.Location = new System.Drawing.Point(148, 196);
+			this.label3.Name = "label3";
+			this.label3.Size = new System.Drawing.Size(115, 20);
+			this.label3.TabIndex = 2;
+			this.label3.Text = "Количество:";
+			// 
+			// textBox1
+			// 
+			this.textBox1.Location = new System.Drawing.Point(285, 129);
+			this.textBox1.Name = "textBox1";
+			this.textBox1.Size = new System.Drawing.Size(258, 20);
+			this.textBox1.TabIndex = 3;
+			this.textBox1.Validating += new System.ComponentModel.CancelEventHandler(this.textBox1_Validating);
+			// 
+			// textBox2
+			// 
+			this.textBox2.Location = new System.Drawing.Point(285, 161);
+			this.textBox2.Name = "textBox2";
+			this.textBox2.Size = new System.Drawing.Size(258, 20);
+			this.textBox2.TabIndex = 4;
+			this.textBox2.Validating += new System.ComponentModel.CancelEventHandler(this.textBox2_Validating);
+			// 
+			// errorProvider1
+			// 
+			this.errorProvider1.ContainerControl = this;
+			// 
+			// maskedTextBox1
+			// 
+			this.maskedTextBox1.Location = new System.Drawing.Point(285, 198);
+			this.maskedTextBox1.Mask = "9999999999";
+			this.maskedTextBox1.Name = "maskedTextBox1";
+			this.maskedTextBox1.Size = new System.Drawing.Size(258, 20);
+			this.maskedTextBox1.TabIndex = 7;
+			this.maskedTextBox1.Validating += new System.ComponentModel.CancelEventHandler(this.maskedTextBox1_Validating);
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(800, 450);
+			this.Controls.Add(this.maskedTextBox1);
+			this.Controls.Add(this.textBox2);
+			this.Controls.Add(this.textBox1);
+			this.Controls.Add(this.label3);
+			this.Controls.Add(this.label2);
+			this.Controls.Add(this.label1);
+			this.Name = "Form1";
+			this.Text = "Form1";
+			((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).EndInit();
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.Label label2;
+		private System.Windows.Forms.Label label3;
+		private System.Windows.Forms.TextBox textBox1;
+		private System.Windows.Forms.TextBox textBox2;
+		private System.Windows.Forms.ErrorProvider errorProvider1;
+		private System.Windows.Forms.MaskedTextBox maskedTextBox1;
+	}
+}
+
---
+++ exzam/exzam/Form1.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace exzam
+{
+	public partial class Form1 : Form
+	{
+		public Form1()
+		{
+			InitializeComponent();
+		}
+
+		private void label1_Click(object sender, EventArgs e)
+		{
+
+		}
+
+		private void textBox1_Validating(object sender, CancelEventArgs e)
+		{
+			if (String.IsNullOrEmpty(textBox1.Text))
+			{
+				errorProvider1.SetError(textBox1, "Не указано имя!");
+			}
+			else if (textBox1.Text.Length < 4)
+			{
+				errorProvider1.SetError(textBox1, "Слишком короткое имя!");
+			}
+			else if (textBox1.Text.Length > 50)
+			{
+				errorProvider1.SetError(textBox1, "Слишком длинное имя!");
+			}
+			else
+			{
+				errorProvider1.Clear();
+			}
+		}
+
+		private void textBox2_Validating(object sender, CancelEventArgs e)
+		{
+
+			if (String.IsNullOrEmpty(textBox2.Text))
+			{
+				errorProvider1.SetError(textBox2, "Не указано имя!");
+			}
+			else if (textBox2.Text.Length < 10)
+			{
+				errorProvider1.SetError(textBox2, "Слишком короткое имя!");
+			}
+			else if (textBox2.Text.Length > 35)
+			{
+				errorProvider1.SetError(textBox2, "Слишком длинное имя!");
+			}
+			else
+			{
+				errorProvider1.Clear();
+			}
+
+		}
+
+		
+
+		private void maskedTextBox1_Validating(object sender, CancelEventArgs e)
+		{
+			if (String.IsNullOrEmpty(maskedTextBox1.Text))
+			{
+				errorProvider1.SetError(maskedTextBox1, "Не указано кол-во!");
+			}
+			
+			else
+			{
+				errorProvider1.Clear();
+			}
+		}
+	}
+	}
+
